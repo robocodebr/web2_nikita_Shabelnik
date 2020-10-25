7d51a2146faa7f35c534ad5bb42cfaa91474895c
robocodebr <ROBOCODEBR@GMAIL.COM>
2020-10-25 19:35:26 +0200

    lesson8

---
+++ lesson 7/assets/js/script.js
@@ -0,0 +1,46 @@
+// let a = 5;
+// if(a>4) {
+//     console.log("a > 4");
+// } else {
+//     console.log("a < 4");
+// }
+
+
+// let a = 5;
+// if((a % 2 == 1) && (a != 3)){
+//     console.log("a - нечётное, a != 3");
+// }
+
+
+// let a = parseInt(prompt("Введите число"));
+// if((a % 2 == 0)) {
+//     console.log("Чётное");
+// } else {
+//     console.log("Не чётное");
+// }
+
+
+// let b1 = true;
+// let b2 = false;
+
+// console.log(b1 || b2);
+// console.log(b1 && b1);
+// console.log(b2 || b1);
+// console.log(b2 && b2);
+
+
+// let b1 = true;
+// let b2 = false;
+
+// console.log(b1 || b2);
+// console.log(b2 || b1);
+// console.log(b1 || b1);
+// console.log(b2 || b2);
+
+let a = 43;
+let b = 14;
+let c = 82;
+
+if(a > b && b > c) {
+    console.log(a || b || c);
+}
