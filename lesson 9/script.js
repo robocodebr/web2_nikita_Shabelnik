f3ac53fa2f0a93f7de436cdeb295a560ea5bdfa3
robocodebr <ROBOCODEBR@GMAIL.COM>
2020-11-08 20:02:42 +0200

    lesson10

---
+++ lesson 9/script.js
@@ -0,0 +1,46 @@
+/*for (i=0;i<10;i++)
+      {
+            console.log(Math.round(Math.random()*100));
+      }
+
+      let names =["Шабельник Никита","Ахмед Мухамедович","Раджат Ашотович"];
+
+      for(let i = 0; i<names.length; i++){
+          console.log(names[i]);
+      }
+      
+       let names =["Комп","Мышка","клавиатура","Монитор","Розетка"];
+       let arr2 = new Array(3); 
+       names [2] = 5
+       arr[0] = 5;
+       for(let i = 0; i<10; i++){
+       arr[i] = Math.floor(Math.random()*10);
+       }
+       for(let i = 0; i<names.length; i++){
+       console.log(names[i]);
+       }
+       console.log(arr);
+       console.log(arr.length);*/
+
+       let names =["Комп","Мышка","клавиатура","Монитор","Розетка"];
+       console.log(names [2]);
+       console.log(names);
+       names.length
+
+       names[5] ="Кресло";
+       names[6] ="Пирамида";
+       names[7] ="z negjq";
+
+    names.pop() ;
+    alert( names.length );
+    names.push("Лодка");
+    alert( names );
+
+    names.shift(z negjq);
+    names.unshift(Kirca)
+
+
+       
+       
+
+    
